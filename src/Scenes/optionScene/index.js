02e297112ea74f9c30bfa79bed858a73ede8900d
BlueDream0718 <104906327+Cheng0718@users.noreply.github.com>
2022-05-26 21:52:26 -0800

    Update index.js

--- src/Scenes/optionScene/index.js
+++ src/Scenes/optionScene/index.js
@@ -115,7 +115,7 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo }, ref) =>
 
         audioList.bodyAudio1.pause()
         audioList.bodyAudio2.pause()
-
+        audioList.buzzAudio.pause();
 
         timerList.map(timer => clearTimeout(timer))
 
@@ -243,8 +243,8 @@ const OptionScene = React.forwardRef(({ nextFunc, transSignaler, _geo }, ref) =>
         getRandomAnswerList()
 
     const judgeFunc = (num) => {
-        audioList.buzzAudio.pause();
-        
+      
+
         if (num == doneCount + correctNum) {
 
             parentObject.current.style.pointerEvents = 'none'
